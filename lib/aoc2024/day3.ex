253aa99dff1b2d8e332320a926c2b854654921ff
Oliver Pile <oliver.pile@bt.com>
2024-12-03 13:00:49 +0000

    Day 3 part 2 (working)

--- lib/aoc2024/day3.ex
+++ lib/aoc2024/day3.ex
@@ -11,35 +11,32 @@ defmodule Aoc2024.Day3 do
     |> Enum.sum()
   end
 
+  # Issue was the the state of do/dont should have been persisted within line changes. So combines all lines together and then ran it.
   def part2() do
-    Aoc2024.get_file_lines("./input/test.txt")
-    |> Enum.map(fn line ->
-      Regex.scan(~r/(?:mul\((\d{1,3}),(\d{1,3})\)|(do\(\))|(don't\(\)))/, line,
-        capture: :all_but_first
-      )
-      |> Enum.reduce({:do, 0}, fn val, {state, count} ->
-        IO.inspect(count, label: :acc)
-        IO.inspect(val)
-        {state_only, new_state} = check_state(val, state) |> IO.inspect()
+    input_str =
+      Aoc2024.get_file_lines("./input/day3.txt")
+      |> Enum.join("\n")
 
-        cond do
-          state_only ->
-            {new_state, count}
+    Regex.scan(~r/(?:mul\((\d{1,3}),(\d{1,3})\)|(do\(\))|(don't\(\)))/, input_str,
+      capture: :all_but_first
+    )
+    |> Enum.reduce({:do, 0}, fn val, {state, count} ->
+      {state_only, new_state} = check_state(val, state)
 
-          new_state == :dont ->
-            {new_state, count}
+      cond do
+        state_only ->
+          {new_state, count}
 
-          true ->
-            [x, y] = val
-            mult = String.to_integer(x) * String.to_integer(y)
-            {new_state, count + mult}
-        end
-      end)
-      |> elem(1)
+        new_state == :dont ->
+          {new_state, count}
+
+        true ->
+          [x, y] = val
+          mult = String.to_integer(x) * String.to_integer(y)
+          {new_state, count + mult}
+      end
     end)
-    |> List.flatten()
-    |> Enum.sum()
-    |> IO.inspect()
+    |> elem(1)
   end
 
   defp check_state(vals, state) do
