749fc0d2a76f81ab3f0e09d91210c03eca7cdeb9
Oliver Pile <oliver.pile443@gmail.com>
2024-12-04 19:24:31 +0000

    Day 4 part 1
    Not my own solution, see https://github.com/irondeau/advent_of_code/blob/master/lib/2024/04_ceres_search.ex.

--- lib/aoc2024/day4.ex
+++ lib/aoc2024/day4.ex
@@ -1,8 +1,49 @@
 defmodule Aoc2024.Day4 do
-  # Elixir doesnt like doing list[x][y]. Whilst I could work around it, I'd rather do it ideomatically. Will come back to this when I can figure that out.
+  # I cant take credit for part 1. It was copied from https://github.com/irondeau/advent_of_code/blob/master/lib/2024/04_ceres_search.ex.
   def part1() do
-    lines =
-      Aoc2024.get_file_lines("./input/test.txt")
-      |> Enum.map(&String.split(&1, "", trim: true))
+    word_map = get_word_map()
+
+    for y <- 0..(map_size(word_map) - 1), x <- 0..(map_size(word_map[y]) - 1) do
+      find_count(word_map, {x, y})
+    end
+    |> Enum.sum()
+  end
+
+  defp find_count(map, {x, y}, [char | rest] \\ ~w[X M A S]) do
+    if map[y][x] == char do
+      [{-1, -1}, {0, -1}, {1, -1}, {-1, 0}, {1, 0}, {-1, 1}, {0, 1}, {1, 1}]
+      |> Enum.map(fn {dx, dy} ->
+        check_run(map, {x + dx, y + dy}, rest, {dx, dy})
+      end)
+      |> Enum.sum()
+    else
+      0
+    end
+  end
+
+  defp check_run(map, {x, y}, ["S"], _dxdy) do
+    if map[y][x] == "S", do: 1, else: 0
+  end
+
+  defp check_run(map, {x, y}, [char | rest], {dx, dy}) do
+    if map[y][x] == char do
+      check_run(map, {x + dx, y + dy}, rest, {dx, dy})
+    else
+      0
+    end
+  end
+
+  defp get_word_map() do
+    Aoc2024.get_file_lines("./input/day4.txt")
+    |> Enum.with_index()
+    |> Enum.map(fn {line, y} ->
+      {y,
+       line
+       |> String.graphemes()
+       |> Enum.with_index()
+       |> Enum.map(fn {char, x} -> {x, char} end)
+       |> Map.new()}
+    end)
+    |> Map.new()
   end
 end
